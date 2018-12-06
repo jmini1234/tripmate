4cda3b50ec9358800f184f5e4cf12d62bb2a00d4
uiuiui629 <uiuiuidrive@gmail.com>
2018-12-06 14:15:21 +0900

    list.html, list.js 수정

--- public/scripts/list.js
+++ public/scripts/list.js
@@ -57,19 +57,17 @@ function loadList() {  // Loads the last 12 messages and listen for new ones.
 }
 
 var List_TEMPLATE =
-  '<div id="list-container" class="mdl-cell mdl-cell--12-col mdl-grid">' +
-      "<li class='mdl-list__item'>"
-        "<span class='mdl-list__item-primary-content'>"
-          "<i class='material-icons mdl-list__item-icon'>chat</i>"
-          "<a class='name' href ='main.html'>go</a>"
-        '</span>'
-      '</li>'
-  '</div>';
+      "<li class='mdl-list__item'>" +
+        "<span class='mdl-list__item-primary-content'>"+
+          "<i class='material-icons mdl-list__item-icon'>chat</i>"+
+          "<a class='name' href ='main.html'>go</a>"+
+        '</span>'+
+      '</li>';
 
 function displayList(roomlist) {
 
   console.log(roomlist);
-
+  roomlist = ["Seoul", "Newyork", "Busan"];
   var htmlelt = document.getElementById("lists");
 
   for(var room in roomlist){
